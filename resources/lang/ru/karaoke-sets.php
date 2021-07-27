bd78dd174976c330e6aed4ad87f7dfc1e7a0d894
Denys Voitenko <den730ws@gmail.com>
2021-07-27 16:53:11 +0300

    занес текст  в языковые переменые

---
+++ resources/lang/ru/karaoke-sets.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    //MAIN
+    'main_title' => 'Караоке-комплекты <br>с микрофонами',
+    'main_subtitle' => 'с микрофонами',
+    'main_txt' => 'Караоке-система и 2 цифровых беспроводных микрофона SE • 200D.',
+ 
+    //SOUNDBAR
+    'soundbar_title' => 'Караоке-комплекты <br>с саундбаром',
+    'soundbar_subtitle' => 'с микрофонами<br>и саундбаром',
+    'soundbar_txt1' => 'Караоке',
+    'soundbar_txt2' => '-система, 2 цифровых беспроводных микрофона и акустическая система для караоке',
+    
+    //STEREOSYSTEM
+    'stereosystem_title' => 'Караоке-комплекты <br>со стереосистемой',
+    'stereosystem_subtitle' => 'с микрофонами<br>и стереосистемой',
+
+    //BASE
+    'base_link' => 'Узнать больше',
+    'base_txt' => 'песен и возможность загрузки песен из каталога на 80 000 караоке-фонограмм.',
+    'base_txt2' => 'Караоке-система, 2 цифровых беспроводных микрофона и акустическая система для караоке',
+
+
+    //ALT
+    'alt_main_with_mic' => 'микрофон для караоке купить',
+    'alt_main_with_mic_mob' => 'микрофон купить для караоке',
+    'alt_main_evobox_grey' => 'серый',
+    'alt_main_evobox_red' => 'красный',
+    'alt_main_evobox_blue' => 'синий',
+    'alt_main_evobox_gold' => 'золотой',
+    'alt_main_evobox_silver' => 'серебрянный',
+    'alt_main_evobox_black' => 'черный',
+    'alt_main_evobox_white' => 'белый',
+    'alt_main_evobox_audio' => 'аудиосистема',
+    'alt_main_evobox_karaoke' => 'караоке',
+];
