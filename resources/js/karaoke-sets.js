7ba9b7a1a95c57342777d0216e1f4725c35a36ca
Denys Voitenko <den730ws@gmail.com>
2021-07-30 10:42:18 +0300

    удалил лишние1

--- resources/js/karaoke-sets.js
+++ resources/js/karaoke-sets.js
@@ -1,23 +1,3 @@
-// $(document).ready(function(){
-//     $('.base_tab-preview').click(function(){
-//         var tab_id = $(this).attr('data-tab');
-//         $(this).closest('.base_container').find('.base_tab-img').removeClass('active');
-//         $(this).closest('.base_container').find('.base_tab-preview').removeClass('active');
-//
-//         $(this).addClass('active');
-//         $("#"+tab_id).addClass('active');
-//     })
-//     $('.evosound_btn').click(function(){
-//         var tab_id = $(this).attr('data-tab');
-//         $(this).closest('.base_container').find('.base_img').removeClass('active');
-//         $(this).closest('.base_container').find('.base_img-mob').removeClass('active');
-//         $(this).closest('.base_container').find('.evosound_btn').removeClass('active');
-//
-//         $(this).addClass('active');
-//         $("."+tab_id).addClass('active');
-//     })
-// })
-
 import { partChangeByClick } from './parts.js';
 
 //PARTS: Табы: смена по клику
